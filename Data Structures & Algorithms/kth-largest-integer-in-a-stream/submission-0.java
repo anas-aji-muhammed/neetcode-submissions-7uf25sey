ec9b06522093e60298d3550a4b6b6bda54244c55
Anas Aji Muhammed <anas.ajeeb2@gmail.com>
2026-08-15 12:54:19 +0100

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,25 @@
+class KthLargest {
+    Queue<Integer> heap;
+    int topK;
+
+
+    public KthLargest(int k, int[] nums) {
+        this.topK = k;
+        heap = new PriorityQueue<>();
+        for(int num: nums){
+            heap.offer(num);
+            if(heap.size()>k){
+                heap.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        heap.offer(val);
+        if(heap.size()>topK){
+            heap.poll();
+        }
+        return heap.stream().sorted().limit(topK).toList().get(0);
+
+    }
+}
